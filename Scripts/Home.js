3d1600e7ad1caa1a78ca92b3cd0929420f6c97a6
JoshuaPBiddle <joshuaphillip.biddle@calbaptist.edu>
2021-07-30 09:51:27 -0700

    Add project files.

---
+++ Scripts/Home.js
@@ -0,0 +1,41 @@
+﻿
+function XMLAlert(event) {
+    event.preventDefault();
+
+    var textInput = document.getElementById("textBox").value;
+    var request = new XMLHttpRequest(),
+        method = "POST",
+        url = "/Text/AddTextRow";
+
+    request.open(method, url, false);
+    request.setRequestHeader("Content-Type", "application/json");
+
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var status = request.status;
+            if (status === 0 || (status >= 200 && status < 400)) { //200s = success, 300s = redirect
+                //alert("request was successful"); 
+
+                //var success = document.getElementById("success");  div insertion
+                //success.innerText = "Request was successful"; div insertion
+
+                var form = document.forms[0];
+                var successDivs = form.querySelectorAll(".success");
+                for (var successDiv of successDivs) {
+                    form.removeChild(successDiv);
+                }
+               
+                var success = document.createElement("div");
+                success.innerText = "Request was successful";
+                success.className = "success";
+                form.appendChild(success);
+            }
+        }
+    };
+
+    //var dbEntry = "{TextInput: '" + textInput + "'}";
+    var dbEntry = JSON.stringify({ TextInput: textInput });
+    request.send(dbEntry);
+}
+
+document.forms[0].addEventListener('submit', XMLAlert);
---
+++ Scripts/app.jsx
@@ -0,0 +1,133 @@
+﻿/// <reference path="jquery-3.4.1.intellisense.js" />
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { listItems: [], name: '', currentItem: null}
+        this.handleContentChange = this.handleContentChange.bind(this);
+    }
+
+    Post = (destination, data, callback) => {
+        const request = new XMLHttpRequest(),
+            method = "POST",
+            url = "https://localhost:44375/Text/" + destination;
+
+        request.open(method, url);
+        request.setRequestHeader("Content-Type", "application/json");
+
+        request.onreadystatechange = function () {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                var status = request.status;
+                if (status === 0 || (status >= 200 && status < 400)) {
+                    if (request.response != null && request.reponse != '') {
+                        callback(JSON.parse(request.response));
+                    } else {
+                        callback();
+                    }
+                }
+            }
+        };
+        let dbEntry = JSON.stringify(data);
+        request.send(dbEntry);
+    }
+
+    fetchList = () => {
+        this.Post("fetchList", null, (items) => {
+            this.setState({ listItems: items.list });
+        })
+    }
+
+    componentDidMount = () => {
+        this.fetchList();
+    };
+
+    fetchItem = (id) => {
+        this.Post("fetchItem", { id: id }, (item) => {
+            this.setState({ currentItem: item });
+        })
+    }
+
+    handleSubmit = (event) => {
+        this.Post("AddTextRow", { Name: this.state.name }, () => {
+            this.fetchList();
+            this.setState({ name: "" });
+        })
+        event.preventDefault();
+    }
+  
+    handleChange = (event) => {
+        this.setState({ name: event.target.value });
+    }
+
+
+    handleContentChange = (event) => {
+        let newItem = { ...this.state.currentItem };
+        newItem.Content = event.target.value;
+        this.setState({ currentItem: newItem });
+    }
+    
+    handleContentSubmit = (event) => {
+        let data = {
+            ID: this.state.currentItem.ID,
+            Content: this.state.currentItem.Content
+        };
+        this.Post("handleContentSubmit", data, () => {
+            this.fetchList();
+            this.setState({ currentItem: null });
+        });
+        event.preventDefault();
+
+    }
+   
+
+    deleteItem = (id) => {
+        this.Post("deleteItem", { id: id }, () => {
+            this.fetchList();
+        })
+    }
+
+    render = () => {
+        const itemRows = this.state.listItems.map(item =>
+            <tr key={item.ID}>
+                <td onClick={() => this.fetchItem(item.ID)}>{item.ID}</td>
+                <td onClick={() => this.fetchItem(item.ID)}>{item.Name}</td>
+                <td> <button onClick={() => this.deleteItem(item.ID)}>Delete</button></td>
+            </tr>);
+
+        let currentDiv = null;
+        if (this.state.currentItem != null) {
+            currentDiv =
+                <div>
+
+                    <form onSubmit={this.handleContentSubmit}>
+                        <textarea value={this.state.currentItem.Content} onChange={this.handleContentChange}> Enter Content </textarea>
+                        <button> Save Changes </button>
+                    </form>
+                </div>;
+        }
+
+        return (
+            <div>
+                <table>
+                    <tbody>
+                        {itemRows}
+                    </tbody>
+                </table>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" id="textBox" value={this.state.name} onChange={this.handleChange} />
+                    <br />
+                    <button> Add New Item </button> 
+                </form>
+                {/*<div contentEditable="true">{this.state.name}</div>*/}
+                <div >{this.state.name}</div>
+
+               {/*<div dangerouslySetInnerHTML={{ __html: this.state.name }}> </div>*/}
+                {currentDiv}
+            </div>
+        )   
+    }
+}
+
+
+
