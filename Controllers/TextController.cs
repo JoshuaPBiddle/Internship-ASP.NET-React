3e151d015e66b67a602f4cc7629c0e4af270872d
JoshuaPBiddle <jpbiddle98@gmail.com>
2021-07-30 09:45:48 -0700

    First Commit

---
+++ Controllers/TextController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMVCApplication.Models;
+
+namespace MyMVCApplication.Controllers
+{
+    public class TextController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult fetchList()
+        {
+            TextDBEntities db = new TextDBEntities();
+
+            var list = db.TextTables.Select(x => new TextModel
+            {
+                ID = x.ID,
+                Name = x.Name
+            }).ToList();
+
+            return Json(new
+            {
+                list = list
+            });
+        }
+
+        [HttpPost]
+        public ActionResult fetchItem(int id)
+        {
+            TextDBEntities db = new TextDBEntities();
+
+            var item = db.TextTables.Select(x => new TextModel
+            {
+                ID = x.ID,
+                Name = x.Name,
+                Content = x.Content
+            }).First(m => m.ID == id);
+
+            return Json(item);
+        }
+
+
+        [HttpPost]
+        public ActionResult addItem(TextModel textModel)
+        {
+            try
+            {
+                TextDBEntities db = new TextDBEntities();
+
+                TextTable textTable = new TextTable();
+
+                textTable.Name = textModel.Name;
+
+                db.TextTables.Add(textTable);
+
+                db.SaveChanges();
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+
+            return Json(textModel);
+        }
+
+        [HttpPost]
+        public ActionResult deleteItem(int id)
+        {
+            TextDBEntities db = new TextDBEntities();
+
+            TextTable textTable = db.TextTables.Find(id);
+
+            db.TextTables.Remove(textTable);
+
+            db.SaveChanges();
+
+            return Json(new { });
+        }
+
+        [HttpPost]
+        public ActionResult handleContentSubmit(TextModel textModel)
+        {
+            TextDBEntities db = new TextDBEntities();
+
+            TextTable textTable = db.TextTables.Find(textModel.ID);
+
+            textTable.Content = textModel.Content;
+
+            db.SaveChanges();
+
+            return Json(textModel);
+        }
+
+        [HttpPost]
+        public ActionResult fetchContent(int id)
+        {
+            TextDBEntities db = new TextDBEntities();
+
+            var content = db.TextTables.Select(x => new TextModel
+            {
+                Content = x.Content
+            }).First(m => m.ID == id);
+
+            return Json(content);
+        }
+    }
+}
---
+++ Scripts/app.jsx
@@ -0,0 +1,157 @@
+﻿/// <reference path="jquery-3.4.1.intellisense.js" />
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: '', listItems: [], currentItem: null}
+        this.handleChange = this.handleChange.bind(this);
+        this.handleContentChange = this.handleContentChange.bind(this);
+    }
+
+    Post = (destination, data, callback) => {
+        const request = new XMLHttpRequest(),
+            method = "POST",
+            url = "https://localhost:44375/Text/" + destination;
+
+        request.open(method, url, true);
+        request.setRequestHeader("Content-Type", "application/json");
+
+        request.onreadystatechange = function () {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                var status = request.status;
+                if (status === 0 || (status >= 200 && status < 400)) {
+                    if (request.response != null && request.response != '') {
+                        callback(JSON.parse(request.response));
+                    } else {
+                        callback();
+                    }
+                }
+            }
+        };
+        let dbEntry = JSON.stringify(data);
+        request.send(dbEntry);
+    }
+
+    componentDidMount = () => {
+        this.fetchList();
+    };
+
+    fetchList = () => {
+        this.Post("fetchList", null, (items) => {
+            this.setState({ listItems: items.list });
+        })
+    }
+
+    fetchItem = (id) => {
+        if (this.state.currentItem === null) {
+            this.Post("fetchItem", { id: id },
+                (item) => {
+                    this.setState({ currentItem: item });
+                })
+        } else {
+            this.setState({ currentItem: null });
+            this.Post("fetchItem", { id: id },
+                (item) => {
+                    this.setState({ currentItem: item });
+                })
+        }
+    } 
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.Post("addItem", { Name: this.state.name }, () => {
+            this.fetchList();
+            this.setState({ name: '' });
+        })
+    }
+
+    handleChange = (event) => {
+        this.setState({ name: event.target.value });
+    }
+
+    handleContentChange = (event) => {
+        let newItem = { ...this.state.currentItem };
+        newItem.Content = event.target.value;
+        this.setState({ currentItem: newItem });
+        this.myComp();
+    }
+
+    handleContentSubmit = (event) => {
+        let data = {
+            ID: this.state.currentItem.ID,
+            Content: this.state.currentItem.Content,
+        };
+
+        this.Post("handleContentSubmit", data, () => {
+            this.fetchList();
+            this.setState({ currentItem: null });
+        });
+        event.preventDefault();
+    }
+
+    deleteItem = (id) => {
+        this.Post("deleteItem", { id: id }, () => {
+            this.fetchList();
+        });
+    }
+
+    exit = () => {
+        this.setState({ currentItem: null });
+    }
+
+    render = () => {
+        
+        const itemRows = this.state.listItems.map(item =>
+            <tr key={item.ID}>
+                <td onClick={() => this.fetchItem(item.ID)}> {item.Name}</td>
+                <td><button class="button" id="deleteButton" onClick={() => this.deleteItem(item.ID)}> <b>x</b> </button></td>
+            </tr>
+        );
+    
+        let currentDiv = null;
+        if (this.state.currentItem != null) {
+            currentDiv =
+                <div class= "content">
+                <h2 id="name">{this.state.currentItem.Name}:</h2>
+                <button class="exit" onClick={this.exit}> X </button>
+                    <form onSubmit={this.handleContentSubmit}>
+                        <textarea id="contentBox" value={this.state.currentItem.Content} onChange={this.handleContentChange}> Enter Content </textarea>
+                        <br />
+                        <button class="button" id="saveContent"> Save Changes </button>
+                </form>
+                </div>
+        }
+
+        let renderedHTML = null;
+        if (this.state.currentItem != null) {
+            renderedHTML =
+                <div id="content" dangerouslySetInnerHTML={{ __html: this.state.currentItem.Content }} />;
+        }
+
+        return (
+            <div class="format">
+                <table>
+                    <tbody>
+                        {itemRows}
+                            <tr>
+                                <td>
+                                    <form onSubmit={this.handleSubmit}>
+                                    <table>
+                                        <br />
+                                        <input  id="textBox" type="text" value={this.state.name} onChange={this.handleChange} />
+                                    </table>
+                                    </form>
+                            </td>
+                            <td> <button onClick={this.handleSubmit} class="button" id="addButton"><b>+</b></button></td>
+                            </tr>
+                    </tbody>
+                </table>
+                {currentDiv}
+                {renderedHTML}
+            </div>
+        )
+    }
+}
+
+
+
